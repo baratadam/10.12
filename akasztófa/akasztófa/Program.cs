2b34c67b298595ed213bec29bd7db111a0b15f7c
Baráth Ádám <b1barada@vasvari.org>
2023-10-12 09:10:01 +0200

    kesz

---
+++ akasztófa/akasztófa/Program.cs
@@ -0,0 +1,13 @@
+﻿using akasztófa;
+
+var target = new Target();
+Console.WriteLine("Kérem adja meg a kitalált szót: ");
+target.Word = Console.ReadLine();
+Console.WriteLine("Kérem adjon meg betűt: ");
+target.SetWord(Console.ReadLine());
+do
+{
+    Console.WriteLine("Írjon be egy betűt");
+    target.SetWord(Console.ReadLine());
+}
+while (true);
---
+++ akasztófa/akasztófa/szavak.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace akasztófa;
+
+public class Target
+{
+    public string Word;
+    private string HashedWord; //*******
+
+    public void SetWord(string word)
+    {
+        Word = word;
+        for (int i = 0; i < word.Length; i++) ;
+        {
+            HashedWord = HashedWord + "*";
+        }
+    }
+    public void CheckCharacter(char character)
+        {
+            for (int i = 0;i < Word.Length;i++) 
+        {
+            if (Word[i] == character);
+        }
+        }
+    }
